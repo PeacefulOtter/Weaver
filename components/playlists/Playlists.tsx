8cb05746dc6dd6e26d89a82694a2b68ae0606c1c
PeacefulOtter <zankro67@gmail.com>
2022-04-18 16:24:06 +0200

    Used less useRouter in the Playlists and added a Home button

--- components/playlists/Playlists.tsx
+++ components/playlists/Playlists.tsx
@@ -1,52 +1,57 @@
 
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { FiHome } from 'react-icons/fi'
 import { useSpotify } from '../../context/SpotifyContext';
 
 import styles from './Playlists.module.css' 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-const Playlist = ( { id, name, image, setPlaylistId, isMinimal } ) => {
-
-	const router = useRouter()
-	const { playlistID } = router.query
-
-	const emoji = name.match(/\p{Emoji}+/gu)[0].replace('2022', '🎵').replace('2021', '🎵')
-	const nameEmoji = name.replace(emoji, '')
-
-	let style = styles.playlistwrapper + ' ' 
-		+ (id === playlistID ? styles.playlistActiveWrapper : '') + ' '
+const PlaylistElt = ( { isActive, onClick, emoji, nameEmoji } ) => {
+	
+	const style = styles.playlistwrapper + ' ' 
+		+ (isActive ? styles.playlistActiveWrapper : '') + ' '
 
 	return (
-		<div className={style} onClick={setPlaylistId(id)}>
+		<div className={style} onClick={onClick}>
 			<div className={styles.playlistEmoji}>{emoji}</div>
 			<div className={styles.playlistName}>{nameEmoji}</div>
 		</div>
 	)
 }
 
+
+const Playlist = ( { isActive, name, onClick } ) => {
+
+	const emoji = name.match(/\p{Emoji}+/gu)[0].replace('2022', '🎵').replace('2021', '🎵')
+	const nameEmoji = name.replace(emoji, '')
+
+	return <PlaylistElt isActive={isActive} onClick={onClick} emoji={emoji} nameEmoji={nameEmoji} />
+}
+
 const Playlists = () => {
 
 	const { playlists } = useSpotify()	
-	const [minimal, setMinimal] = useState<boolean>(true);
 
 	const router = useRouter()
+	const { playlistID } = router.query
 
 	const setPlaylistId = (id: string) => () => {
 		router.push('/' + id)
 	}
 
-	const enter = () => setMinimal(false)
-	const leave = () => setMinimal(true)
-
 	return (
-		<div className={styles.playlists} onMouseEnter={enter} onMouseLeave={leave}>
+		<div className={styles.playlists} >
+			<PlaylistElt 
+				isActive={playlistID === undefined} 
+				onClick={setPlaylistId('')} 
+				emoji={<FiHome />} 
+				nameEmoji="Home" 
+			/>
 			{ playlists.map( pl => 
 				<Playlist 
 					{...pl} 
-					isMinimal={minimal}	
-					setPlaylistId={setPlaylistId} 
+					isActive={pl.id === playlistID}
+					onClick={setPlaylistId(pl.id)} 
 					key={`playlist${Math.random()}`} />
 			) } 
 		</div>
